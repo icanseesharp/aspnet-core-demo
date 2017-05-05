6d6c1dd529d55f09d95952f96453cc404434f0a4
icanseesharp <sagar.yerva@digitaslbi.com>
2017-05-05 19:16:00 +0530

    implemented WriteData option using GET method

--- IoTDemo.API/Controllers/IoTDataController.cs
+++ IoTDemo.API/Controllers/IoTDataController.cs
@@ -137,7 +137,7 @@ namespace IoTDemo.API.Controllers
         {
             if(!IsValidKey(key))
             {
-                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, could not auhtenticate the user with given key"));
+                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, could not auhtenticate the user with given key - The key is either invalid or disabled"));
             }
             if (!ModelState.IsValid)
             {
@@ -154,6 +154,83 @@ namespace IoTDemo.API.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(ioTData);
+        }
+
+
+        [HttpGet]
+        [Route("[Controller]/writedata")]
+        public async Task<IActionResult> WriteData(string name, string value, string date, string key)
+        {
+            #region check if the key is provided and valid             
+            if (!IsValidKey(key))
+            {
+                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, could not auhtenticate the user with given key - The key is either invalid or disabled"));
+            }
+            #endregion
+
+            var ioTData = new IoTData();
+
+            #region validate the provided data
+
+            var existingName = string.IsNullOrEmpty(name) ? false : _context.IoTDataNames.Where(q => q.Name.ToLowerInvariant() == name.ToLowerInvariant()).Count() > 0 ? true : false;
+            if (existingName)
+            {
+                ioTData.IoTDataNameId = _context.IoTDataNames.Where(q => q.Name.ToLowerInvariant() == name.ToLowerInvariant()).First().Id;
+            }
+            else if (!string.IsNullOrEmpty(name))
+            {
+                var newIotDataName = _context.Add(new IoTDataName()
+                {
+                    Name = name,
+                    IoTKeyId = _context.IoTKeys.Where(q => q.Key == Guid.Parse(key)).First().Id
+                });
+                ioTData.IoTDataNameId = newIotDataName.Entity.Id;
+            }
+
+            else
+            {
+                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, name field was not provided or was empty"));
+            }
+
+            var ParsedDate = new DateTime();
+            if (string.IsNullOrEmpty(date))
+            {
+                ioTData.Date = DateTime.Now;
+            }
+            else if (DateTime.TryParse(date, out ParsedDate))
+            {
+                ioTData.Date = ParsedDate;
+            }
+            else
+            {
+                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, Provided date is invalid, please check the date"));
+            }
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, Please provide a mandatory floating point value"));
+            }
+            var ioTValue = new float();
+            if (float.TryParse(value, out ioTValue))
+            {
+                ioTData.Value = ioTValue;
+            }
+            else
+            {
+                return new BadRequestObjectResult(new BadRequestResultModel("Operation failed, Provided Value is not a valid floating point number"));
+            }
+            #endregion
+
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.IoTData.Add(ioTData);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIoTData", new { id = ioTData.Id }, ioTData);
         }
 
         private bool IoTDataExists(int id)
